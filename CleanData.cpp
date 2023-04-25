d8d08850c0672e2c71cb5879092206d8f7dae47b
RobertKilkenny <robertkilkenny@ufl.edu>
2023-04-25 17:05:56 -0400

    Removed unnecessary header file

--- CleanData.cpp
+++ CleanData.cpp
@@ -1,4 +1,13 @@
-#include "CleanData.h"
+#include <fstream>
+#include <iostream>
+#include <random>
+#include <string>
+#include <vector>
+#include <map>
+using namespace std;
+
+string GetPiece(ifstream* input);
+void CleanData();
 
 void CleanData()
 {
